4ecc3a594c5c8ce6e29cd702c85e7317c06a829a
AlfredoMartinezMartinez <alfredo.martinez1@alu.uclm.es>
2018-03-07 00:43:41 +0100

    agregado servicios de productos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import {app_routing} from './app.routes';
 //servicios
 
 import {InformacionService} from "./services/informacion.service";
+import {ProductosService} from './services/productos.service';
 
 
 //componentes
@@ -33,7 +34,7 @@ import { ProductoComponent } from './components/producto/producto.component';
     app_routing,
     HttpModule
   ],
-  providers: [InformacionService],
+  providers: [InformacionService,ProductosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
