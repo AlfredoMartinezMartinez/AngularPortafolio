90d95bf67332f5ff141659fb1c6b367ef2256e91
AlfredoMartinezMartinez <alfredo.martinez1@alu.uclm.es>
2018-03-07 01:03:39 +0100

    Productos enlazados de firebase

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -17,7 +17,7 @@ export class ProductosService {
       this.http.get("https://paginaweb-324a7.firebaseio.com/productos_idx.json").subscribe(res =>{
         console.log(res.json());
         this.cargando= false;
-        // this.productos=res.json();
+        this.productos=res.json();
       })
     // }
   }
