fcdd449a0d68553eefbb4b7c596f64a1ec882ad9
AlfredoMartinezMartinez <alfredo.martinez1@alu.uclm.es>
2018-03-07 02:36:54 +0100

    Pagina de busqueda casi funcionando

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { RouterModule, Routes } from '@angular/router';
-import {AboutComponent,ProductoComponent,PortafolioComponent} from './components/index.paginas';
+import {AboutComponent,ProductoComponent,PortafolioComponent,SearchComponent} from './components/index.paginas';
 
 const app_routes: Routes = [
   { path: 'home', component: PortafolioComponent },
   { path: 'about', component: AboutComponent },
   { path: 'producto/:id', component: ProductoComponent },
+  { path: 'buscar/:termino', component: SearchComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'home' },
 ];
 
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -11,17 +11,17 @@ export class ProductosService {
        this.cargar_productos();
      }
 
+  public cargar_producto(cod:string){
+    return this.http.get(`https://paginaweb-324a7.firebaseio.com/productos/${cod}.json`);
+  }
+
   public cargar_productos(){
     this.cargando= true;
-    // if(this.productos.length===0){
-      this.http.get("https://paginaweb-324a7.firebaseio.com/productos_idx.json").subscribe(res =>{
-        // console.log(res.json());      
+    this.http.get("https://paginaweb-324a7.firebaseio.com/productos_idx.json").subscribe(res =>{
+        // console.log(res.json());
         this.cargando= false;
         this.productos=res.json();
-
-
-      })
-    // }
+      }) ;
   }
 
 
